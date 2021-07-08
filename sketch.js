f8477bbc259879081294227da25d6373d8530766
Vinayak Hakhu <39513628+thegammingttl@users.noreply.github.com>
2021-07-08 19:32:04 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var box_H ; box_Li; box_Na; box_K; box_Rb; box_Cs; box_Fr;
+var box_Be; box_Mg; box_Ca; box_Sr; box_Ba; box_Ra
+var box_Sc; box_Y; 
+var box_Ti; box_Zr; box_Hf; box_Rf; 
+var box_V; box_Nb; box_Ta; box_Db
+var box_Cr; box_Mo; box_W; box_Sg
+var box_Mn; box_Tc; box_Re; box_Bh
+var box_Fe; box_RU; box_Os; box_Hs 
+var box_Co; box_Rh; box_Ir; box_Mt
+var box_Ni; box_Pd; box_Pt; box_Ds
+var box_Cu; box_Ag; box_Au; box_Rg;
+var box_Zn; box_Cd; box_Hg; box_Cn;
+var box_B; box_Ai; box_Ga; box_In; box_Ti; box_Uut;
+var box_C; box_Si; box_Ge; box_Sn; box_Pb; box_Fi;
+var box_N; box_P; box_As; box_Sb; box_Bi; box_Uup;
+var box_F; box_Cl; box_Br; box_I; box_At; box_Uus;
+var box_He; box_Ne; box_Ar; box_Kr; box_Xe; box_Rn; box_Uuo
+var box_Lanthanide_Series; box_La; box_Ce; box_Pr; box_Nd; box_Pm; box_Sm;
+var box_Eu; box_Gd; box_Tb; box_Dy; box_Ho; box_Er; box_Tm; box_Yb; box_Lu;
+var box_Actinide_Series; box_Ac; box_Th; box_Pa; box_U; box_Np; box_Pu; box_Am;
+var box_Cm; box_Bk; box_Cf; box_Es; box_Fm; box_Md; box_No; box_Lr 
+var box_Lables; box_red; box_orange; box_yellow; box_green;
+var box_light_blue; box_blue; box_light_purple; box_purple;
+var box_light_green; box_dark_green; 
+var box_creator; var big_box_info;
+
+function setup() {
+    createCanvas(1920, 1080);
+}
+function draw(){
+background("blue")
+
+
+
+
+
+ drawsprites()
+}
